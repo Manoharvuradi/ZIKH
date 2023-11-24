7d114bdaaab3578e1b23b95d34c4bf119e5263f4
Manohar_reddie <manoharvuradi5@gmail.com>
2023-11-25 00:21:53 +0530

    removed react-google-api and added opensource react-leaflet

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,2 +1,9 @@
-import Home from "Y/components/Home";
-export default Home;
+import dynamic from "next/dynamic";
+const DynamicHeader = dynamic(() => import("../components/map"), {
+    ssr: false
+});
+export default function Home() {
+    return (
+        <DynamicHeader />
+    )
+}
