01b79bd18df054901d1c1b3c114f9e8424beb2d0
Manohar_reddie <manoharvuradi5@gmail.com>
2023-11-25 19:02:49 +0530

    added user current location

--- src/components/map/index.tsx
+++ src/components/map/index.tsx
@@ -1,6 +1,25 @@
-import React from 'react'
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import React, { useState } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+
+function LocationMarker() {
+   const [position, setPosition] = useState<any>(null);
+   const map = useMapEvents({
+      click() {
+         map.locate()
+      },
+      locationfound(e) {
+         setPosition(e.latlng)
+         map.flyTo(e.latlng, map.getZoom())
+      },
+   })
+   return position === null ? null : (
+      <Marker position={position}>
+         <Popup>You are here</Popup>
+      </Marker>
+   )
+}
+
 const Map = () => {
    return (
       <MapContainer style={{ height: "100vh" }} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
@@ -8,11 +27,7 @@ const Map = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
          />
-         <Marker position={[51.505, -0.09]}>
-            <Popup>
-               A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-         </Marker>
+         <LocationMarker />
       </MapContainer>
    )
 }
