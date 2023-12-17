2e2626e34d5938cd7f5ba5b98002c7c7fd527547
Manohar_reddie <manoharvuradi5@gmail.com>
2023-12-18 02:09:09 +0530

    model issue solved and env issue imported successfully

--- src/common/buttons/filledButton.tsx
+++ src/common/buttons/filledButton.tsx
@@ -13,6 +13,8 @@ export default function Button(props: any) {
     const { text, onClick, disabled, className, type } = props;
     return (
         <button
+            className={`flex h-8 items-center justify-center gap-2 rounded-md bg-primary-blue px-2.5 text-sm text-white transition duration-300 hover:bg-hover-blue ${className || ""
+                } ${disabled ? "cursor-not-allowed opacity-50" : ""}`}
             disabled={disabled}
             type={type ? type : "button"}
             onClick={onClick ? () => onClick() : () => { }}
--- src/utils/helpers/index.ts
+++ src/utils/helpers/index.ts
@@ -1,6 +1,5 @@
-import { env } from "process"
+import { env } from "../../env.mjs"
 
 export const generateLocation = (lat: number, lng: number) => {
     const location = `https://api.opencagedata.com/geocode/v1/json?q=${lat}+${lng}&key=${env.API_KEY}`
-    console.log("location", location);
 }
