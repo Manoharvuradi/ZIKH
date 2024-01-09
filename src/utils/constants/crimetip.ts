bada0ec86366599394a2ab194c42db0119f099fd
Manohar_reddie <manoharvuradi5@gmail.com>
2024-01-09 13:30:28 +0530

    Added crime tip component

---
+++ src/utils/constants/crimetip.ts
@@ -0,0 +1,26 @@
+export const crimeTipInputs = [
+    {
+        label:"Address or location",
+        name: "location",
+        type: "text",
+        required: true
+    },
+    {
+        label: "City",
+        name: "city",
+        type: "text",
+        required: true
+    },
+    {
+        label: "State",
+        name: "state",
+        type: "text",
+        required: true
+    },
+    {
+        label: "Zip",
+        name: "zip",
+        type: "number",
+        required: true
+    },
+]
