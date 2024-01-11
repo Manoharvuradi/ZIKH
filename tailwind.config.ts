002853f82f3e467c06360d573ab11c22d00eb5ee
Manohar_reddie <manoharvuradi5@gmail.com>
2024-01-11 19:00:52 +0530

    added single input and button, updated tsconfig file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,86 @@
 import { type Config } from "tailwindcss";
 
 export default {
+  darkMode: ['class'],
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  container: {
+    center: true,
+    padding: '2rem',
+    screens: {
+      '2xl': '1400px',
+    },
+  },
   theme: {
+    fontFamily: {
+      sans: ['Unica77, sans-serif', { fontFeatureSettings: "'ss04' on" }],
+    },
     extend: {
       colors: {
-        "primary-blue": "#008FE0"
-      }
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        muted: {
+          DEFAULT: 'var(--muted)',
+          foreground: 'var(--muted-foreground)',
+        },
+        primary: {
+          DEFAULT: 'var(--primary)',
+          muted: 'var(--primary-muted)',
+          foreground: 'var(--primary-foreground)',
+          'foreground-muted': 'var(--primary-foreground-muted)',
+        },
+        secondary: {
+          DEFAULT: 'var(--secondary)',
+          muted: 'var(--secondary-muted)',
+          foreground: 'var(--secondary-foreground)',
+          'foreground-muted': 'var(--secondary-foreground-muted)',
+        },
+        destructive: {
+          DEFAULT: 'var(--destructive)',
+          muted: 'var(--destructive-muted)',
+          foreground: 'var(--destructive-foreground)',
+          'foreground-muted': 'var(--destructive-foreground-muted)',
+        },
+        accent: {
+          DEFAULT: 'var(--accent)',
+          muted: 'var(--accent-muted)',
+          foreground: 'var(--accent-foreground)',
+          'foreground-muted': 'var(--accent-foreground-muted)',
+        },
+        popover: {
+          DEFAULT: 'var(--popover)',
+          muted: 'var(--popover-muted)',
+          foreground: 'var(--popover-foreground)',
+          'foreground-muted': 'var(--popover-foreground-muted)',
+        },
+        card: {
+          DEFAULT: 'var(--card)',
+          muted: 'var(--card-muted)',
+          foreground: 'var(--card-foreground)',
+          'foreground-muted': 'var(--card-foreground-muted)',
+        },
+        borderRadius: {
+          lg: 'var(--radius)',
+          md: 'calc(var(--radius) - 2px)',
+          sm: 'calc(var(--radius) - 4px)',
+        },
+        keyframes: {
+          'accordion-down': {
+            from: { height: '0' },
+            to: { height: 'var(--radix-accordion-content-height)' },
+          },
+          'accordion-up': {
+            from: { height: 'var(--radix-accordion-content-height)' },
+            to: { height: '0' },
+          },
+        },
+        animation: {
+          'accordion-down': 'accordion-down 0.2s ease-out',
+          'accordion-up': 'accordion-up 0.2s ease-out',
+        },
+      },
     },
   },
   plugins: [],
