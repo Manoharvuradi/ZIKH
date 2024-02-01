d34432ae95f1c8aeba783c41ab8f84a7888429ba
Manohar_reddie <manoharvuradi5@gmail.com>
2024-02-01 23:46:00 +0530

    fixed loader and fixed button

--- src/components/loader/index.tsx
+++ src/components/loader/index.tsx
@@ -1,5 +1,11 @@
 import { Spinner } from "@material-tailwind/react";
 
 export function CustomSpinner() {
-    return <Spinner className="h-16 w-16 text-gray-900/50" />;
+    return(
+        <div className="fixed inset-0 z-50 flex items-center justify-center bg-opacity-50">
+            <Spinner
+                className="h-16 w-16 text-blue-900/50"
+            />;
+        </div>
+    )
 }
