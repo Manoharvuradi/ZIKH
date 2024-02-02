8c38f8c82c3a105c61c14bae6e6be77c83b8b087
Manohar_reddie <manoharvuradi5@gmail.com>
2024-02-02 07:46:05 +0530

    added searchByState and searchByCity tables in schema with migrations

--- src/pages/browe-by-state/index.tsx
+++ src/pages/browe-by-state/index.tsx
@@ -1,8 +1,17 @@
+import { UpdatedButton } from 'Y/common/buttons/button';
 import React from 'react'
 
 const BrowseByState = () => {
+  const handleCityAdd = () => {
+    console.log('city add');
+  }
   return (
-    <div>index</div>
+    <div>
+      <h1 className='text-3xl text-center mt-5 font-bold'>Browse by state</h1>
+      <UpdatedButton type="submit" className="bg-red-900 text-white mx-auto hover:text-black hover:bg-white" variant={'outline'} onClick={handleCityAdd}>
+        Add city
+      </UpdatedButton>
+    </div>
   )
 }
 
