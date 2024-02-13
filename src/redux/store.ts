7b8f7681ac115f6b5f87574c4dd70bb3ad2b0943
Manohar_reddie <manoharvuradi5@gmail.com>
2024-02-13 23:49:01 +0530

    adding redux in zikh-app

---
+++ src/redux/store.ts
@@ -0,0 +1,15 @@
+
+import {
+    createStore,
+    applyMiddleware,
+    combineReducers,
+    AnyAction,
+    Store,
+} from "redux";
+
+export interface IState {
+
+}
+const reducer = (state: IState | undefined, action: AnyAction) => {
+
+}
