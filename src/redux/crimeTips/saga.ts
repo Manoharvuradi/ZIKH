d1de26ae5a745d85ecb28258b0d4f37ee79dd34b
Manohar_reddie <manoharvuradi5@gmail.com>
2024-02-15 19:16:05 +0530

    writing handler for the crimeTip and made changes in saga

--- src/redux/crimeTips/saga.ts
+++ src/redux/crimeTips/saga.ts
@@ -1,10 +1,13 @@
 import { Action } from "redux";
 import { all, put, takeLatest } from "redux-saga/effects";
 import { crimeTipActions, getCrimeTipDetailsFailure } from "./acrions";
+import axios, { AxiosResponse } from "axios";
 
 function* submitCrimeTip(action: Action<any>): Generator {
     try{
-
+        const payload = action;
+        const response: AxiosResponse | any = yield axios.post("",payload,{baseURL:"/"});
+        // if(response.)
     }catch(error){
         yield put(
             getCrimeTipDetailsFailure("Something went wrong")
